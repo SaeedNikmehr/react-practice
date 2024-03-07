c071a3c3dceddea9bd25d6cd7f651089d49e8afd
saeed <hovalkafi@gmail.com>
2024-03-07 15:41:48 +0330

    trim files

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
+    element: <Home/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
   },
 ]);
 
